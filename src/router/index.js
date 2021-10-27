42bb34c08a6c6f97fcd433c23ee57f9d2cf6ed36
Clearlove-lzp <1587029518@qq.com>
2021-10-27 18:32:19 +0800

    裁剪

--- src/router/index.js
+++ src/router/index.js
@@ -176,7 +176,7 @@ Vue.use(VueRouter)
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "cropper" */ '../views/cropper.vue')
+    component: () => import(/* webpackChunkName: "cropper" */ '../views/cropper/index.vue')
   },
 ]
 
